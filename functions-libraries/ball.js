56068060797194b6980e85d675474e19e8af52a7
Kay <abdifiiq@gmail.com>
2021-11-03 23:25:02 -0500

    loops and function
    I did some looping on large datas to recreate a painting. also some ball movements on the screen

---
+++ functions-libraries/ball.js
@@ -0,0 +1,23 @@
+var list = [];
+
+list.push(create());
+list.push(create());
+list.push(create());
+list.push(create());
+
+var i = 0;
+undefined
+var length = list.length;
+
+while(i < length){
+    var ball = list[i];
+    colorRandom(ball);
+    i++
+}
+
+
+while(i < length){
+    var ball = list[i];
+    colorRandom(ball);
+    i++
+};
---
+++ functions-objects/ball.js
@@ -0,0 +1,8 @@
+var length = data.length;
+
+// you work goes here
+// -----------------------
+for(let i =0; i < length; i++){
+    let ball = data[i];
+    create(ball.x, ball.y, ball.color)
+}
